5fbd8d957fffcbc5d2ad1c41605a5babba10aff8
WayneYuq <3154816042@qq.com>
2020-06-29 22:27:05 +0800

    Update to 0.4 version

--- include/myslam/mappoint.h
+++ include/myslam/mappoint.h
@@ -29,17 +29,38 @@ class MapPoint
  public:
   typedef shared_ptr<MapPoint> Ptr;
   unsigned long id_; // ID
+  static unsigned long factory_id_;
+  bool good_;
   Vector3d pos_; // Position in world
   Vector3d norm_; // Normal of viewing direction
   Mat descriptor_; // Descriptor for matching
-  int observed_times_; // being observed by feature matching algo.
-  int correct_times_; // being an inliner in pose estimation
 
+  list<Frame*> observed_frames_; // key-frames that can observe this point
+  
+  int matched_times_; // being an inliner in pose estimation
+  int visible_times_; // being visible in current frame
+  
   MapPoint();
-  MapPoint( long id, Vector3d position, Vector3d norm );
+  MapPoint(
+    unsigned long int id, 
+    const Vector3d& position, 
+    const Vector3d& norm,
+    Frame* frame=nullptr,
+    const Mat& descriptor = Mat()
+  );
 
+  inline cv::Point3f getPositionCV() const {
+    return cv::Point3f( pos_(0, 0), pos_(1, 0), pos_(2, 0) );
+  }
+  
   // factory function
   static MapPoint::Ptr createMapPoint();
+  static MapPoint::Ptr createMapPoint(
+    const Vector3d& pos_world,
+    const Vector3d& norm_,
+    const Mat& descriptor,
+    Frame* frame
+  );
 };
 
 }
